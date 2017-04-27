d90c29a78349b2d0162d850edeb3b42c4944d072
KSivixay <saeng.sivixay@gmail.com>
2017-04-27 10:52:08 -0600

    updated more css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var books = [
     description: "One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha'\s Weekly Shōnen Jump magazine since July 19, 1997, with the chapters collected into eighty-four tankōbon volumes to date.",
     author: "Eiichiro Oda",
     publish: 1997,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
      },
   {
@@ -15,6 +16,7 @@ var books = [
     description: "One-Punch Man is an ongoing Japanese superhero parody webcomic created by an author using the pseudonym One which began publication in early 2009. The series quickly went viral, surpassing 7.9 million hits in June 2012.",
     author: "One",
     publish: 2012,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
      },
   {
@@ -24,6 +26,7 @@ var books = [
     description: "Sword Art Online is a 2009 Japanese light novel series written by Reki Kawahara and illustrated by Abec. The series takes place in the near future and focuses on various virtual reality MMORPG worlds.",
     author: "Reki Kawahara",
     publish: 2009,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
      },
   {
@@ -33,6 +36,7 @@ var books = [
     description: "Tokyo Ghoul is a Japanese dark fantasy manga series by Sui Ishida.",
     author: "Sui Ishida",
     publish: 2011,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
      },
   {
@@ -42,6 +46,7 @@ var books = [
     description: "Grimgar of Fantasy and Ash is a Japanese light novel series written by Ao Jūmonji and illustrated by Eiri Shirai.",
     author: "Ao Jumonji",
     publish: 2016,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
   },
   {
@@ -51,6 +56,7 @@ var books = [
     description: "God Eater is an anime adaptation of the God Eater video game series. It is animated by Ufotable and began airing on July 12, 2015 after a one-week delay.",
     author: "Takayuki Hirao",
     publish: 2015,
+    watchOnline: "https://www.google.com",
     lastRead: new Date()
      }
 
@@ -70,7 +76,9 @@ $(document).ready(function(){
     $('#title').html('Title: ' + currentBook.title);
     $('#author').html('Author: ' + currentBook.author);
     $('#publish').html('Published: ' + currentBook.publish);
-    $('#description').html('Description: ' + currentBook.description)
+    $('#description').html('Description: ' + currentBook.description);
+    $('#watchOnline').attr('href', currentBook.watchOnline);
+    $('#watchOnline').html('Watch Online Here');
     $('#lastRead').html('Last Read: ' + currentBook.lastRead);
   })
 });
